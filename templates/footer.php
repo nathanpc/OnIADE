8893aeca978abffd11d701e8c1d55a1063c8c654
Nathan Campos <eeepc904@gmail.com>
2020-12-26 15:17:02 +0000

    Moved the basic building blocks to templates

---
+++ templates/footer.php
@@ -0,0 +1,9 @@
+	<!-- Footer -->
+	<footer class="container">
+		<hr>
+		<div class="container">
+			<p>Embrace. Extend. Extinguish.</p>
+		</div>
+	</footer>
+</body>
+</html>
---
+++ templates/navbar.php
@@ -0,0 +1,23 @@
+<!-- Navigation Bar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<div class="container">
+		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+
+		<a class="navbar-brand" href="#"><?= $config->app->name ?></a>
+		<div class="collapse navbar-collapse" id="navbar">
+			<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<li class="nav-item">
+					<a class="nav-link active" aria-current="page" href="#">Home</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="/debugview.php">Debug</a>
+				</li>
+				<!--<li class="nav-item">
+					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				</li>-->
+			</ul>
+		</div>
+	</div>
+</nav>
