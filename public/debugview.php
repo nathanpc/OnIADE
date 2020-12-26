5259bd4f54465e1b96c87c002f00cb428cfd6f29
Nathan Campos <eeepc904@gmail.com>
2020-12-26 18:30:30 +0000

    Showing all of the 'smallish' tables we have so far in the debug view

--- public/debugview.php
+++ public/debugview.php
@@ -6,8 +6,37 @@
 			Floors
 			<small class="text-muted">The things we stand on</small>
 		</h3>
-
 		<?= Database::get_html_table("floors") ?>
+
+		<h3>
+			Rooms
+			<small class="text-muted">The things we have class in</small>
+		</h3>
+		<?= Database::get_html_table("rooms") ?>
+
+		<h3>
+			Accounts
+			<small class="text-muted">People that have signed up</small>
+		</h3>
+		<?= Database::get_html_table("accounts") ?>
+
+		<h3>
+			Device Types
+			<small class="text-muted">What sizes do we have?</small>
+		</h3>
+		<?= Database::get_html_table("device_types") ?>
+
+		<h3>
+			Operating Systems
+			<small class="text-muted">We all know Linux is superior</small>
+		</h3>
+		<?= Database::get_html_table("operating_systems") ?>
+
+		<h3>
+			Devices
+			<small class="text-muted">Everything that has ever passed by us</small>
+		</h3>
+		<?= Database::get_html_table("devices") ?>
 	</div>
 
 <?php require(__DIR__ . "/../templates/footer.php"); ?>
