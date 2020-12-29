1059c8d05d51b5a1731a9bd3469ce0ad6908d20f
Nathan Campos <eeepc904@gmail.com>
2020-12-29 13:45:24 +0000

    Checking for required parameters in different stages in the floor API endpoint

--- public/api/v1/floor.php
+++ public/api/v1/floor.php
@@ -15,8 +15,24 @@ header("Content-Type: application/json");
  * Check if we got the required parameters and automatically fails if they are
  * not present.
  */
-function check_required_params() {
-	if ($_SERVER["REQUEST_METHOD"] != "GET") {
+function check_required_params($stage = null) {
+	if ($_SERVER["REQUEST_METHOD"] == "GET") {
+		if ($stage == "specific_floor") {
+			// Getting a specific floor.
+			if (!(isset($_GET["id"]) || isset($_GET["number"]))) {
+				http_response_code(424);
+				echo json_encode(array(
+					"info" => array(
+						"level" => 0,
+						"status" => "error",
+						"message" => "Required parameter 'id' or 'number' wasn't specified."
+					)
+				));
+
+				exit(1);
+			}
+		}
+	} else {
 		// User requested with a method that is not supported by us.
 		http_response_code(400);
 		echo json_encode(array(
@@ -31,9 +47,7 @@ function check_required_params() {
 	}
 }
 
-// Check if we have everything we need.
 check_required_params();
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	// Check if the user wants a list of the floors.
 	if (!isset($_GET["id"]) && !isset($_GET["number"])) {
@@ -59,6 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 	}
 
 	// User wants information about a floor.
+	check_required_params("specific_floor");
 	$floor = null;
 	if (isset($_GET["id"])) {
 		$floor = Floor::FromID($_GET["id"]);
