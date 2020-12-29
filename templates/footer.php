8ca35734c114013ddcd4d063eba6f43f036097d6
Nathan Campos <eeepc904@gmail.com>
2020-12-29 15:58:57 +0000

    Updated the footer template

--- templates/footer.php
+++ templates/footer.php
@@ -2,7 +2,15 @@
 	<footer class="container">
 		<hr>
 		<div class="container">
-			<p>Embrace. Extend. Extinguish.</p>
+			<div class="row">
+				<div class="col">
+					OnIADE &#169; 2020-<?= date("Y") ?> <a href="https://innoveworkshop.com/">Innove Workshop</a>
+				</div>
+
+				<div class="col" style="text-align: right;">
+					Designed and built by <a href="https://nathancampos.me/">@nathanpc</a> and <a href="https://www.instagram.com/digobraga8/">@digobraga8</a>
+				</div>
+			</div>
 		</div>
 	</footer>
 </body>
