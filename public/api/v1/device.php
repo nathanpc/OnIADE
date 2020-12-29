ad8eeb895315982b1566d154cc13e0406b830a31
Nathan Campos <eeepc904@gmail.com>
2020-12-29 14:14:52 +0000

    Organized the device API endpoint

--- public/api/v1/device.php
+++ public/api/v1/device.php
@@ -11,69 +11,62 @@ require_once(__DIR__ . "/../../../src/Device.php");
 require_once(__DIR__ . "/../../../src/Floor.php");
 require_once(__DIR__ . "/../../../src/HistoryEntry.php");
 
-// Set the content type.
 header("Content-Type: application/json");
-
-/**
- * Check if we got the required parameters and automatically fails if they are
- * not present.
- */
-function check_required_params() {
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		// We've found a device and want to add it to the history.
-		if (!(isset($_POST["macaddr"]) && isset($_POST["floor"]) && isset($_POST["ipaddr"]))) {
-			http_response_code(424);
-			echo json_encode(array(
-				"info" => array(
-					"level" => 0,
-					"status" => "error",
-					"message" => "Required parameter 'macaddr', 'ipaddr', or 'floor' wasn't specified."
-				)
-			));
-
-			exit(1);
-		}
-	} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
-		// We want some information about a device.
-		if (!(isset($_GET["id"]) || isset($_GET["macaddr"]))) {
-			http_response_code(424);
-			echo json_encode(array(
-				"info" => array(
-					"level" => 0,
-					"status" => "error",
-					"message" => "Required parameter 'id' or 'macaddr' wasn't specified."
-				)
-			));
-
-			exit(1);
-		}
-	} else {
-		// User requested with a method that is not supported by us.
-		http_response_code(400);
-		echo json_encode(array(
-			"info" => array(
-				"level" => 0,
-				"status" => "error",
-				"message" => "Invalid request method " . $_SERVER["REQUEST_METHOD"] . "."
-			)
-		));
-
-		exit(1);
-	}
-}
-
-// Check if we have everything we need.
 check_required_params();
-
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-	// User wants information about a device.
 	$device = null;
+
+	// Check if we just want a device list.
+	if (!isset($_GET["id"]) && !isset($_GET["macaddr"])) {
+		list_devices();
+		return;
+	}
+
+	// User wants information about a device.
+	check_required_params("get_device");
 	if (isset($_GET["id"])) {
 		$device = Device::FromID($_GET["id"]);
 	} else if (isset($_GET["macaddr"])) {
 		$device = Device::FromMACAddress($_GET["macaddr"]);
 	}
 
+	// Send device information.
+	get_device($device);
+} else if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Edge device wants to add a device found in the network to the history.
+	add_device_entry();
+}
+
+/**
+ * Lists all of the available devices.
+ */
+function list_devices() {
+	// Build the base response.
+	http_response_code(200);
+	$response = array(
+		"info" => array(
+			"level" => 2,
+			"status" => "ok",
+			"message" => "Devices found in database."
+		),
+		"list" => array()
+	);
+
+	// Go through devices adding them as arrays to the response.
+	foreach (Device::List() as $floor) {
+		array_push($response["list"], $floor->as_array());
+	}
+
+	// Send response.
+	echo json_encode($response);
+}
+
+/**
+ * Sends information about a specific device.
+ * 
+ * @param Device $device Device to show information about.
+ */
+function get_device($device = null) {
 	// Check if we didn't get a device.
 	if (is_null($device)) {
 		http_response_code(400);
@@ -98,9 +91,16 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 		),
 		"device" => $device->as_array()
 	);
+
+	// Send device information.
 	echo json_encode($response);
-} else if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	// Edge device wants to add a device found in the network to the history.
+}
+
+/**
+ * Adds a device entry to the history and also adds the device to the database
+ * if it doesn't exist.
+ */
+function add_device_entry() {
 	$device = null;
 	$floor = null;
 	
@@ -146,6 +146,58 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
 		),
 		"entry" => $entry->as_array()
 	));
+}
+
+/**
+ * Check if we got the required parameters and automatically fails if they are
+ * not present.
+ * 
+ * @param string $stage Specific stage we need to check parameters for.
+ */
+function check_required_params($stage = null) {
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// We've found a device and want to add it to the history.
+		if (!(isset($_POST["macaddr"]) && isset($_POST["floor"]) && isset($_POST["ipaddr"]))) {
+			http_response_code(424);
+			echo json_encode(array(
+				"info" => array(
+					"level" => 0,
+					"status" => "error",
+					"message" => "Required parameter 'macaddr', 'ipaddr', or 'floor' wasn't specified."
+				)
+			));
+
+			exit(1);
+		}
+	} else if ($_SERVER["REQUEST_METHOD"] == "GET") {
+		if ($stage == "get_device") {
+			// We want some information about a device.
+			if (!(isset($_GET["id"]) || isset($_GET["macaddr"]))) {
+				http_response_code(424);
+				echo json_encode(array(
+					"info" => array(
+						"level" => 0,
+						"status" => "error",
+						"message" => "Required parameter 'id' or 'macaddr' wasn't specified."
+					)
+				));
+
+				exit(1);
+			}
+		}
+	} else {
+		// User requested with a method that is not supported by us.
+		http_response_code(400);
+		echo json_encode(array(
+			"info" => array(
+				"level" => 0,
+				"status" => "error",
+				"message" => "Invalid request method " . $_SERVER["REQUEST_METHOD"] . "."
+			)
+		));
+
+		exit(1);
+	}
 }
 
 ?>
