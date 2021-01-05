acaaa787a6d5de78bf0df409784e04189e4d8b37
Nathan Campos <eeepc904@gmail.com>
2021-01-05 15:55:36 +0000

    Started working on adding icons to objects

---
+++ src/Device/Icon.php
@@ -0,0 +1,103 @@
+<?php
+/**
+ * Icon.php
+ * Abstraction class for a Font Awesome icon.
+ *
+ * @author Nathan Campos <nathan@innoveworkshop.com>
+ */
+
+namespace OnIADE\Device;
+require __DIR__ . "/../../vendor/autoload.php";
+use PDO;
+
+class Icon {
+	private $name;
+	private $color;
+
+	/**
+	 * Device icon object constructor.
+	 * 
+	 * @param string $name  Icon name.
+	 * @param string $color Icon color.
+	 */
+	public function __construct($name = null, $color = null) {
+		$this->name = $name;
+		$this->color = $color;
+	}
+
+	/**
+	 * Gets the Font Awesome tag for this icon.
+	 * 
+	 * @param  boolean $with_color Should we have the color added?
+	 * @return string              Font Awesome tag with color if asked for.
+	 */
+	public function get_tag($with_color = true) {
+		$html = "";
+
+		if ($with_color)
+			$html .= "<span style=\"color: " . $this->get_color() . "\">";
+
+		$html .= "<i class=\"" . $this->get_name() . "\"></i>";
+
+		if ($with_color)
+			$html .= "</span>";
+
+		return $html;
+	}
+
+	/**
+	 * Gets the icon name.
+	 * 
+	 * @return string Icon name.
+	 */
+	public function get_name() {
+		if (is_null($this->name))
+			return "fas fa-times";
+
+		return $this->name;
+	}
+
+	/**
+	 * Sets the icon name.
+	 * 
+	 * @param string $name Icon name.
+	 */
+	public function set_name($name) {
+		$this->name = $name;
+	}
+
+	/**
+	 * Gets the icon color.
+	 * 
+	 * @return string Icon color.
+	 */
+	public function get_color() {
+		if (is_null($this->color))
+			return "#000000";
+
+		return $this->color;
+	}
+
+	/**
+	 * Sets the icon color.
+	 * 
+	 * @param string $color Icon color.
+	 */
+	public function set_color($color) {
+		$this->color = $color;
+	}
+
+	/**
+	 * Array representation of this object. Perfect for use in JSON responses.
+	 * 
+	 * @return array Array representation of this object.
+	 */
+	public function as_array() {
+		return array(
+			"name" => $this->name,
+			"color" => $this->color
+		);
+	}
+}
+
+?>
--- templates/head.php
+++ templates/head.php
@@ -13,6 +13,9 @@
 	<link async href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 	<script defer async src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 
+	<!-- Font Awesome -->
+	<script src="https://kit.fontawesome.com/ae0b599810.js" crossorigin="anonymous"></script>
+
 	<!-- Custom stuff -->
 	<script type="text/javascript" src="/assets/js/main.js"></script>
 </head>
