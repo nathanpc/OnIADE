864caf47921983471bb08389263d369de92ff93b
Nathan Campos <eeepc904@gmail.com>
2021-01-11 16:55:24 +0000

    Our first graph

--- public/assets/js/stats.js
+++ public/assets/js/stats.js
@@ -4,20 +4,79 @@
  *
  * @author Nathan Campos <nathan@innoveworkshop.com>
  */
-"use strict";
+ "use strict";
 
 /**
  * Statistics object constructor.
- *
- * @param JSON initialData General data about the system.
  */
-function Statistics(initialData) {
-	this.initialData = initialData;
+function Statistics() {
+	this.base_url = "/api/v1/stats.php";
 }
 
 /**
- * Populates the floor details view with a bunch of graphs.
- */
+* Populates the floor details view with a bunch of graphs.
+*/
 Statistics.prototype.populateFloorsDetail = function () {
-	console.log(this.initialData);
+	var parent = this;
+
+	this.getFloors(function (floors) {
+		floors = JSON.parse(floors);
+		console.log(floors);
+
+		floors = floors.floors;
+		parent.graphFloorOccupancy(floors);
+	});
+}
+
+/**
+* Gets information about the floors.
+* 
+* @param {Function} callback Callback function after we got the floors.
+*/
+Statistics.prototype.getFloors = function (callback) {
+	var http = new HTTP(this.base_url);
+	http.get({ info: "floors" }, callback);
+}
+
+/**
+ * Creates a graph of how crowded each floor is.
+ * 
+ * @param {Object} floors List of information on each of the floors.
+ */
+Statistics.prototype.graphFloorOccupancy = function (floors) {
+	var ctx = document.getElementById("floors-occupation").getContext("2d");
+	var labels = [];
+	var data = [];
+
+	// Go through floors.
+	floors.forEach(function (floor) {
+		labels.push(floor.name);
+		data.push(floor.entries.length);
+	});
+
+	// Setup and graph the data.
+	var chart = new Chart(ctx, {
+		type: "horizontalBar",
+		data: {
+			labels: labels,
+			datasets: [
+				{
+					label: "Total",
+					data: data,
+					borderWidth: 1
+				}
+			],
+		},
+		options: {
+			scales: {
+				yAxes: [
+					{
+						ticks: {
+							beginAtZero: true
+						}
+					}
+				]
+			}
+		}
+	});
 }
