9e0f3a395abb48f4d97b6f13deba60df6b26afb0
Nathan Campos <eeepc904@gmail.com>
2021-01-11 18:38:52 +0000

    Graphing devices and their types in each floor

--- public/assets/js/stats.js
+++ public/assets/js/stats.js
@@ -44,14 +44,86 @@ Statistics.prototype.getFloors = function (callback) {
  * @param {Object} floors List of information on each of the floors.
  */
 Statistics.prototype.graphFloorOccupancy = function (floors) {
+	var cls = this;
 	var ctx = document.getElementById("floors-occupation").getContext("2d");
 	var labels = [];
-	var data = [];
 
-	// Go through floors.
+	// Setup the datasets.
+	var datasets = [
+		{
+			type_key: null,
+			label: "Total",
+			data: [],
+			borderWidth: 1,
+			backgroundColor: [],
+			borderColor: []
+		},
+		{
+			type_key: "mobile",
+			label: "Smartphones",
+			data: [],
+			borderWidth: 1,
+			backgroundColor: [],
+			borderColor: []
+		},
+		{
+			type_key: "desktop",
+			label: "Computers",
+			data: [],
+			borderWidth: 1,
+			backgroundColor: [],
+			borderColor: []
+		},
+		{
+			type_key: "bot",
+			label: "Servers",
+			data: [],
+			borderWidth: 1,
+			backgroundColor: [],
+			borderColor: []
+		}
+	];
+
+	// Go through floors gathering data.
 	floors.forEach(function (floor) {
-		labels.push(floor.name);
-		data.push(floor.entries.length);
+		var id = floor.id;
+		var name = floor.name;
+		var count = 0;
+		var color = null;
+
+		// Add floor to the label and push the totals dataset while we are at it.
+		labels.push(name);
+		datasets[0].data.push(floor.entries.length);
+		datasets[0].backgroundColor.push("#0000AA33");
+		datasets[0].borderColor.push("#0000AA");
+
+		// Count different device types.
+		for (var i = 1; i < datasets.length; i++) {
+			cls.countDeviceType(floor, datasets[i].type_key, datasets[i]);
+		}
+
+		// Normalize colors.
+		datasets.forEach(function (dataset) {
+			var color = null;
+
+			// Try to get a valid color.
+			dataset.borderColor.forEach(function (borderColor) {
+				if (borderColor !== null) {
+					color = borderColor;
+					return;
+				}
+			});
+
+			// Did we actually get anything useful?
+			if (color === null) 
+				color = "#000000";
+
+			// Go back setting all the colors correctly now.
+			for (var i = 0; i < dataset.borderColor.length; i++) {
+				dataset.borderColor[i] = color;
+				dataset.backgroundColor[i] = color + "33";
+			}
+		});
 	});
 
 	// Setup and graph the data.
@@ -59,13 +131,7 @@ Statistics.prototype.graphFloorOccupancy = function (floors) {
 		type: "horizontalBar",
 		data: {
 			labels: labels,
-			datasets: [
-				{
-					label: "Total",
-					data: data,
-					borderWidth: 1
-				}
-			],
+			datasets: datasets
 		},
 		options: {
 			scales: {
@@ -79,4 +145,38 @@ Statistics.prototype.graphFloorOccupancy = function (floors) {
 			}
 		}
 	});
+}
+
+/**
+ * Counts devices by type for the floor occupancy chart.
+ * 
+ * @param  {Object} floor   A floor object.
+ * @param  {String} key     Device type key.
+ * @param  {Array}  dataset Dataset to put the data in.
+ */
+Statistics.prototype.countDeviceType = function (floor, key, dataset) {
+	var count = 0;
+	var color = null;
+
+	// Go through entries.
+	floor.entries.forEach(function (entry) {
+		// Check if we have a device type.
+		if (entry.device.type !== null) {
+			// Check if it matches our search.
+			if (entry.device.type.key === key) {
+				// Count it up.
+				count++;
+
+				// Check if we need to set the color.
+				if (color === null)
+					color = entry.device.type.icon.color;
+			}
+		}
+	});
+
+	dataset.data.push(count);
+	dataset.borderColor.push(color);
+	dataset.backgroundColor.push(color + "33");
+	if (color === null)
+		dataset.backgroundColor.push(null);
 }
