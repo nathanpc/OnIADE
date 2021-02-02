da079d8fa1c15c648c4fc3ee5a9fad79dd3cce41
Nathan Campos <eeepc904@gmail.com>
2021-02-02 14:30:14 +0000

    Added some styling to the device popovers

--- templates/device_popover.php
+++ templates/device_popover.php
@@ -1,15 +1,29 @@
 <?php require __DIR__ . "/../vendor/autoload.php"; ?>
 
-<ul>
+<ul class='device-poplist'>
 	<li>Device Number: <?= $device->get_id() ?></li>
 	<li>MAC Address: <?= $device->get_mac_address() ?></li>
 	<li>Last IP Address: <?= $entry->get_ip_addr() ?></li>
+
+	<?php if ((!is_null($device->get_type())) || 
+			(!is_null($device->get_os()))) { ?>
+		<li><br></li>
+	<?php } ?>
 
 	<?php if (!is_null($device->get_type())) { ?>
 		<li>Type: <?= $device->get_type()->as_text(true) ?></li>
 	<?php } ?>
 
+	<?php if (!is_null($device->get_os())) { ?>
+		<li>Operating System: <?= $device->get_os()->as_text(true) ?></li>
+	<?php } ?>
+
 	<?php if (!is_null($device->get_model())) { ?>
+		<?php if ((!is_null($device->get_model()->get_manufacturer())) ||
+				(!is_null($device->get_model()->get_name()))) { ?>
+			<li><br></li>
+		<?php } ?>
+
 		<?php if (!is_null($device->get_model()->get_manufacturer())) { ?>
 			<li>Manufacturer: <?= $device->get_model()->get_manufacturer() ?></li>
 		<?php } ?>
@@ -17,9 +31,5 @@
 		<?php if (!is_null($device->get_model()->get_name())) { ?>
 			<li>Model: <?= $device->get_model()->get_name() ?></li>
 		<?php } ?>
-	<?php } ?>
-
-	<?php if (!is_null($device->get_os())) { ?>
-		<li>Operating System: <?= $device->get_os()->as_text(true) ?></li>
 	<?php } ?>
 </ul>
