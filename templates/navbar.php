9435f775f71984693f087579eb105ac26560a7c4
Nathan Campos <eeepc904@gmail.com>
2021-02-02 10:57:38 +0000

    Added a dark theme to the navbar

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,5 +1,5 @@
 <!-- Navigation Bar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 	<div class="container">
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
